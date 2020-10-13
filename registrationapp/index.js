e988243256fdfc071d4acb9ffbe37f0d32590ed7
nick <notetrrael@mas.sd>
2020-10-13 16:00:05 +0000

    Get user registration working.

--- registrationapp/index.js
+++ registrationapp/index.js
@@ -1,21 +1,61 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
+const { execSync } = require('child_process');
 const app = express();
 const PORT = 8080;
 
 
 app.use(bodyParser.json());
 
-app.post('/login',(req,res)=>{
-	if(!('username' in req.body.username) || !('password' in req.body.password))
+app.post('/register',async (req,res)=>{
+	if(!('username' in req.body) || !('password' in req.body))
 		return res.send({
 			success:false,
-			error:'Invalid request'
+			message:'Invalid request'
 		});
-	//validate username is alphanumeric
-	
+	if(req.body.username === '')
+		return res.send({
+			success:false,
+			message:'Username cannot be blank.'
+		});
+	if(req.body.password === '')
+		return res.send({
+			success:false,
+			message:'Password cannot be blank.'
+		});
 
+	//validate username is alphanumeric
+	if(!(/^[a-z0-9]+$/i.test(req.body.username)))
+		return res.send({
+			success:false,
+			message:'Invalid username. Must be alphanumeric.'
+		});
+	if(!(/^[a-z0-9]+$/i.test(req.body.password)))
+		return res.send({
+			success:false,
+			message:'Invalid password. Must be alphanumeric. Sorry.'
+		});
+	res.send(await addUser(req.body.username,req.body.password));
 });
+
+
+
+const addUser = (username, password)=>{
+	const userExists = fs.readFileSync('/etc/passwd').toString().split('\n').map(line=>line.split(':')[0]).includes(username);
+	if(userExists)
+		return {
+			success:false,
+			message:'User already exists!'
+		}
+	execSync(`printf "${password}\n${password}\n\n\n\n\n\n\n" | adduser ${username}`);
+	return {
+		success: true,
+		message: 'Registered user!'
+	}
+
+
+}
 
 app.listen(PORT,()=>{
 	console.log(`Listening on port ${PORT}.`);
