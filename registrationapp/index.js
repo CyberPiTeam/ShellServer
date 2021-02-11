ee641d550abc8606a789c7e0d9c85291e2bf40a0
nick <nick@imnotputtingmyemailonthisbotnetofasite.gov>
2021-02-11 21:54:00 +0000

    Overhaul registration code to work a lot better.

--- registrationapp/index.js
+++ registrationapp/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
-const { execSync } = require('child_process');
+const { spawn } = require('child_process');
 const app = express();
 const PORT = 8080;
 
@@ -31,24 +31,39 @@ app.post('/register',async (req,res)=>{
 			success:false,
 			message:'Invalid username. Must be alphanumeric.'
 		});
-	if(!(/^[a-z0-9]+$/i.test(req.body.password)))
+	/*if(!(/^[a-z0-9]+$/i.test(req.body.password)))
 		return res.send({
 			success:false,
 			message:'Invalid password. Must be alphanumeric. Sorry.'
-		});
-	res.send(addUser(req.body.username,req.body.password));
+		});*/
+	res.send(await addUser(req.body.username,req.body.password));
 });
 
 
 
-const addUser = (username, password)=>{
+const callAddUser = (username,password)=>{
+	return new Promise((resolve,reject)=>{
+		const proc = spawn('adduser',['--add_extra_groups',username])
+		proc.stdin.write(`${password}\n${password}\n\n\n\n\n\n\n`);
+		proc.stdin.end();
+		//proc.stdout.on('data',a=>console.log('stdout: ',a.toString()));
+		//proc.stderr.on('data',a=>console.log('stderr: ',a.toString()));
+		proc.on('close', (code) => {
+			resolve();
+		});
+	});
+}
+
+
+const addUser = async (username, password)=>{
 	const userExists = fs.readFileSync('/etc/passwd').toString().split('\n').map(line=>line.split(':')[0]).includes(username);
 	if(userExists)
 		return {
 			success:false,
 			message:'User already exists!'
 		}
-	execSync(`printf "${password}\n${password}\n\n\n\n\n\n\n" | adduser --add_extra_groups ${username}`);
+	await callAddUser(username,password);
+	//execSync(`printf "${password}\n${password}\n\n\n\n\n\n\n" | adduser --add_extra_groups ${username}`);
 	fs.writeFileSync(`/home/${username}/.hushlogin`,'');
 	return {
 		success: true,
